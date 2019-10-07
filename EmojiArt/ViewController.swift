4c96b83ab8cac53f2b31cb3be40d2ccc87961e9e
Emma Walker <walkere@tcd.ie>
2019-10-07 15:45:45 +0100

    Initial Commit

---
+++ EmojiArt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EmojiArt
+//
+//  Created by Emma Walker - TVandMobile Platforms - Core Engineering on 07/10/2019.
+//  Copyright © 2019 Emma Walker - TVandMobile Platforms - Core Engineering. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
