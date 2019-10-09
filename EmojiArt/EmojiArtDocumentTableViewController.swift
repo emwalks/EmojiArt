64bbfdf4ba9ab76396d86dcbcfc80f9dbc04f9b2
Emma Walker <walkere@tcd.ie>
2019-10-09 15:30:37 +0100

    End of L11. Table view with add and delete working

--- EmojiArt/EmojiArtDocumentTableViewController.swift
+++ EmojiArt/EmojiArtDocumentTableViewController.swift
@@ -30,12 +30,18 @@ class EmojiArtDocumentTableViewController: UITableViewController
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "DocumentCell", for: indexPath)
 
-        cell.textLabel? = emojiArtDocuments[indexPath.row]
+        cell.textLabel?.text = emojiArtDocuments[indexPath.row]
         
         return cell
     }
 
 
+    @IBAction func newEmojiArt(_ sender: UIBarButtonItem) {
+        emojiArtDocuments += ["Untitled".madeUnique(withRespectTo: emojiArtDocuments)]
+        tableView.reloadData()
+    }
+    
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
@@ -44,17 +50,18 @@ class EmojiArtDocumentTableViewController: UITableViewController
     }
     */
 
-    /*
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
+            emojiArtDocuments.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
+    
 
     /*
     // Override to support rearranging the table view.
