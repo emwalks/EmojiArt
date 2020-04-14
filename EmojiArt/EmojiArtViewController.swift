6547983a844de55c05ff9796bb4dec9ce00c501e
Emma Walker <walkere@tcd.ie>
2020-04-14 16:15:54 +0100

    adds ability to drag items in collection view

--- EmojiArt/EmojiArtViewController.swift
+++ EmojiArt/EmojiArtViewController.swift
@@ -8,10 +8,10 @@
 
 import UIKit
 
-class EmojiArtViewController: UIViewController, UIDropInteractionDelegate, UIScrollViewDelegate, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout
+class EmojiArtViewController: UIViewController, UIDropInteractionDelegate, UIScrollViewDelegate, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout, UICollectionViewDragDelegate
 {
  
-    
+
     //by making the dropZone separate to the emoji art view we can keep track of what's being dropped in at the controller level but could be the same UIView
     @IBOutlet weak var dropZone: UIView! {
         didSet {
@@ -70,6 +70,8 @@ class EmojiArtViewController: UIViewController, UIDropInteractionDelegate, UIScr
         didSet{
             emojiCollectionView.dataSource = self
             emojiCollectionView.delegate = self
+            emojiCollectionView.dragDelegate = self
+            
         }
     }
     
@@ -91,6 +93,34 @@ class EmojiArtViewController: UIViewController, UIDropInteractionDelegate, UIScr
         }
         return cell
      }
+    
+    //the collection view func has indexPAth already
+    func collectionView(_ collectionView: UICollectionView, itemsForBeginning session: UIDragSession, at indexPath: IndexPath) -> [UIDragItem] {
+        return dragItems(at: indexPath)
+     }
+    
+    //this function allows you drag multiple items
+    func collectionView(_ collectionView: UICollectionView, itemsForAddingTo session: UIDragSession, at indexPath: IndexPath, point: CGPoint) -> [UIDragItem] {
+        return dragItems(at: indexPath)
+    }
+    
+    //here we are going to drag around NSAttributed strings
+    //cell for item is like cell for row at in a table view
+    private func dragItems(at indexPath: IndexPath) -> [UIDragItem] {
+        //here we get the NSAttributed string from the label you have selected (cellForItem) from the collection view
+        //then we create a drag item with the item of type NSItemProvider that has a constructor where you can pass an object
+        if let attributedString = (emojiCollectionView.cellForItem(at: indexPath) as? EmojiCollectionViewCell)?.label.attributedText {
+            let dragItem = UIDragItem(itemProvider: NSItemProvider(object: attributedString))
+            //if you are not dragging outside the app (local) then you don't need to worry about setting async stuff like in drop
+            //less item provider stuff
+            dragItem.localObject = attributedString
+            return [dragItem]
+        } else {
+            //if there has been a problem capturing the string don't set a drag item
+            return []
+        }
+        
+    }
     
     // MARK: - DropZone
     
