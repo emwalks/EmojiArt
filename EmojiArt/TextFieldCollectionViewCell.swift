c4267494a97f65fd3338ab9434a0ac8d6e79e55d
Emma Walker <walkere@tcd.ie>
2020-04-21 15:02:49 +0100

    create custom class for textField collection view cell

---
+++ EmojiArt/TextFieldCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  TextFieldCollectionViewCell.swift
+//  EmojiArt
+//
+//  Created by Emma Walker - TVandMobile Platforms - Core Engineering on 21/04/2020.
+//  Copyright © 2020 Emma Walker - TVandMobile Platforms - Core Engineering. All rights reserved.
+//
+
+import UIKit
+
+class TextFieldCollectionViewCell: UICollectionViewCell {
+    
+}
